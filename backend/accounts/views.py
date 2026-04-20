677fb216af13ddf0db68b4efd5cf7a470c631bc2
amaan <amanmulla231@gmail.com>
2026-04-20 23:01:39 +0530

    Register_view  API

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate, get_user_model
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+def get_token_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        tokens = get_token_for_user(user)
+        return Response({
+            'user':  UserSerializer(user).data,
+            'tokens': tokens,
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
