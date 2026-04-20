ed39e0b953abce266d18e21ce25086e3fcbb8935
amaan <amanmulla231@gmail.com>
2026-04-20 17:25:49 +0530

    class UserManage

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password='None', **extra_fields):
